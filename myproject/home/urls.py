7f2c30818e007c74767aa492ae81a825b5c24f5d
Joshua-Melvin-2024-aia <melvinjoshua777@gmail.com>
2025-02-22 10:50:31 +0530

    New commit

--- myproject/home/urls.py
+++ myproject/home/urls.py
@@ -4,7 +4,7 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    
+
     path('login/nurse/', views.nurse_login, name='nurse_login'),
     path('login/patient/', views.patient_login, name='patient_login'),
     path('',views.homepage,name='homepage'),
